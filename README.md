84a1a7c46d8c5a10cf83e502a0fc79d295167d62
saurabhv749 <saurabhv749@gmail.com>
2021-07-07 18:43:40 +0530

    updated README.md

---
+++ README.md
@@ -0,0 +1,7 @@
+# [File Metadata Microservice](https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/file-metadata-microservice)
+
+> A Simple API which can tell about the file you upload 🙂
+
+## API PREVIEW
+
+![preview](https://drive.google.com/uc?&id=1Mv02kO7fcFskXfDuLcqFaOAxF_ptjo7M)
---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var cors = require('cors');
+const multer = require('multer')
+require('dotenv').config()
+
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse',
+multer().single('upfile'),
+function(req,res){
+  if(req.file){
+      const {originalname,mimetype,size} = req.file
+    res.json({name:originalname,type:mimetype,size}) 
+    }
+    else
+     res.send('Please upload a file first.')
+      
+})
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
+});
